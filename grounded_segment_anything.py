96364623eca356172840dee307c02a48efb5a996
Andy Luhrs <aluhrs@outlook.com>
2023-05-20 22:56:11 -0700

    get grounded SAM working in node

--- grounded_segment_anything.py
+++ grounded_segment_anything.py
@@ -1,14 +1,7 @@
 # Apache
 # Based on https://github.com/IDEA-Research/Grounded-Segment-Anything/blob/08c6b6118eb203bdeefd154203766f1053d03f5a/automatic_label_simple_demo.py
-# Step 1 - Mostly copy/pasting things into the right places:
-#    - Imports at the top
-#    - Args into inputs
-#    - Functions into the class
-#    - __main__ into invoke()
-#    - add self. to variables that need it.
 # python -m pip install -e segment_anything
 # python -m pip install -e GroundingDINO
-# git submodule update --init --recursive
 
 """
 Note: Had to modify line 243 of groundingdino/util/inference.py to:
@@ -39,7 +32,8 @@ from invokeai.app.invocations.image import ImageField, ImageOutput, ImageType
 import cv2
 import numpy as np
 import supervision as sv
-from typing import List
+from PIL import Image as im
+
 
 import torch
 import torchvision
@@ -47,92 +41,68 @@ import torchvision
 from .GroundingDINO.groundingdino.util.inference import Model
 from .segment_anything.segment_anything import sam_model_registry, SamPredictor
 
+
 class GroundedSegmentAnythingInvocation(BaseInvocation):
     """Use grounded segment anything to make a mask - https://github.com/IDEA-Research/Grounded-Segment-Anything"""
     #fmt: off
     type: Literal["grounded_segment_anything"] = "grounded_segment_anything"
     config_file: str = Field(default="E:\\StableDiffusion\\GroundingDINO\\GroundingDINO_SwinT_OGC.py", description="path to config file")  # change the path of the model config file
-    tag2text_checkpoint: str = Field(default="E:\\StableDiffusion\\Tag2Text\\tag2text_swin_14m.pth", description="path to checkpoint file")
     grounded_checkpoint: str = Field(default="E:\\StableDiffusion\\GroundingDINO\\groundingdino_swint_ogc.pth", description="path to checkpoint filet")
+    encoder_version: Literal[tuple(["vit_h", "vit_l", "vit_b"])] = Field(default="vit_h", description="description")
     sam_checkpoint: str = Field(default="E:\\StableDiffusion\\SegmentAnything\\sam_vit_h_4b8939.pth", description="path to checkpoint file")
     image: ImageField = Field(default=None, description="The image to run inference on.")
-    split: str = Field(default=",", description="split for text prompt")
-    output_dir: str = Field(default="E:\\StableDiffusion", description="output directory")
-    box_threshold: float = Field(default=0.25, description="box threshold")
-    text_threshold: float = Field(default=0.2, description="text treshold")
-    iou_threshold: float = Field(default=0.5, description="iou threshold")
-    cpu_only: bool = Field(default=True, description="running on cpu only!, default=False")
+    classes: str = Field(default="wall, door", description="comma separated list of things to classify")
+    box_threshold: float = Field(default=0.35, description="box threshold")
+    text_threshold: float = Field(default=0.25, description="text treshold")
+    nms_threshold: float = Field(default=0.5, description="NMS threshold")
     #fmt: on
 
-    def invoke(self, context: InvocationContext) -> ImageOutput:        
-        # make dir
-        # load image
-        initial_image = context.services.images.get(
-            # self.image.image_type, self.image.image_name
-            "results", "Background1.JPG"
+    def invoke(self, context: InvocationContext) -> ImageOutput:
+        device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        CLASSES = ("unknown,"+self.classes).split(",")
+
+        init_image1 = context.services.images.get(
+            self.image.image_type, self.image.image_name
         )
 
+        init_image = np.array(init_image1)
 
-        DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-        print(f"Using device: {DEVICE}")
-        # GroundingDINO config and checkpoint
-        GROUNDING_DINO_CONFIG_PATH = self.config_file
-        GROUNDING_DINO_CHECKPOINT_PATH = self.grounded_checkpoint
-
-        # Segment-Anything checkpoint
-        SAM_ENCODER_VERSION = "vit_h"
-        SAM_CHECKPOINT_PATH = self.sam_checkpoint
-
-        # Building GroundingDINO inference model
-        grounding_dino_model = Model(model_config_path=GROUNDING_DINO_CONFIG_PATH, model_checkpoint_path=GROUNDING_DINO_CHECKPOINT_PATH)
-
-        # Building SAM Model and SAM Predictor
-        sam = sam_model_registry[SAM_ENCODER_VERSION](checkpoint=SAM_CHECKPOINT_PATH)
+        grounding_dino_model = Model(
+            model_config_path=self.config_file, model_checkpoint_path=self.grounded_checkpoint)
+        sam = sam_model_registry[self.encoder_version](
+            checkpoint=self.sam_checkpoint).to(device=device)
         sam_predictor = SamPredictor(sam)
-
-
-        # Predict classes and hyper-param for GroundingDINO
-        SOURCE_IMAGE_PATH = "D:\\StableDiffusion\\Outputs\\images\\results\\Background1.JPG"
-        CLASSES = ['unknown', 'door', 'bookshelf', 'picture', 'toy']
-        BOX_THRESHOLD = 0.35
-        TEXT_THRESHOLD = 0.25
-        NMS_THRESHOLD = 0.8
-
-        # load image
-        image = cv2.imread(SOURCE_IMAGE_PATH)
 
         # detect objects
         detections = grounding_dino_model.predict_with_classes(
-            image=image,
+            image=init_image,
             classes=CLASSES,
-            box_threshold=BOX_THRESHOLD,
-            text_threshold=TEXT_THRESHOLD
+            box_threshold=self.box_threshold,
+            text_threshold=self.text_threshold
         )
 
         # annotate image with detections
         box_annotator = sv.BoxAnnotator()
         labels = [
-            f"{CLASSES[class_id]} {confidence:0.2f}" 
-            for _, _, confidence, class_id, _ 
+            f"{CLASSES[class_id]} {confidence:0.2f}"
+            for _, _, confidence, class_id, _
             in detections]
-        annotated_frame = box_annotator.annotate(scene=image.copy(), detections=detections, labels=labels)
+        annotated_frame = box_annotator.annotate(
+            scene=init_image.copy(), detections=detections, labels=labels)
 
         # save the annotated grounding dino image
         cv2.imwrite("groundingdino_annotated_image.jpg", annotated_frame)
 
         # NMS post process
-        print(f"Before NMS: {len(detections.xyxy)} boxes")
         nms_idx = torchvision.ops.nms(
-            torch.from_numpy(detections.xyxy), 
-            torch.from_numpy(detections.confidence), 
-            NMS_THRESHOLD
+            torch.from_numpy(detections.xyxy),
+            torch.from_numpy(detections.confidence),
+            self.nms_threshold
         ).numpy().tolist()
 
         detections.xyxy = detections.xyxy[nms_idx]
         detections.confidence = detections.confidence[nms_idx]
         detections.class_id = detections.class_id[nms_idx]
-
-        print(f"After NMS: {len(detections.xyxy)} boxes")
 
         # Prompting SAM with detected boxes
         def segment(sam_predictor: SamPredictor, image: np.ndarray, xyxy: np.ndarray) -> np.ndarray:
@@ -147,11 +117,10 @@ class GroundedSegmentAnythingInvocation(BaseInvocation):
                 result_masks.append(masks[index])
             return np.array(result_masks)
 
-
         # convert detections to masks
         detections.mask = segment(
             sam_predictor=sam_predictor,
-            image=cv2.cvtColor(image, cv2.COLOR_BGR2RGB),
+            image=cv2.cvtColor(init_image, cv2.COLOR_BGR2RGB),
             xyxy=detections.xyxy
         )
 
@@ -159,11 +128,13 @@ class GroundedSegmentAnythingInvocation(BaseInvocation):
         box_annotator = sv.BoxAnnotator()
         mask_annotator = sv.MaskAnnotator()
         labels = [
-            f"{CLASSES[class_id]} {confidence:0.2f}" 
-            for _, _, confidence, class_id, _ 
+            f"{CLASSES[class_id]} {confidence:0.2f}"
+            for _, _, confidence, class_id, _
             in detections]
-        annotated_image = mask_annotator.annotate(scene=image.copy(), detections=detections)
-        annotated_image = box_annotator.annotate(scene=annotated_image, detections=detections, labels=labels)
+        annotated_image = mask_annotator.annotate(
+            scene=init_image.copy(), detections=detections)
+        annotated_image = box_annotator.annotate(
+            scene=annotated_image, detections=detections, labels=labels)
 
         # save the annotated grounded-sam image
         cv2.imwrite("grounded_sam_annotated_image.jpg", annotated_image)
@@ -176,6 +147,7 @@ class GroundedSegmentAnythingInvocation(BaseInvocation):
         metadata = context.services.metadata.build_metadata(
             session_id=context.graph_execution_state_id, node=self
         )
-
-        context.services.images.save(image_type, image_name, annotated_image, metadata)
-        return ImageOutput(image=ImageField(image_type=image_type, image_name=image_name))
+        ret_image = im.fromarray(annotated_image)
+        context.services.images.save(
+            image_type, image_name, ret_image, metadata)
+        return ImageOutput(image=ImageField(image_type=image_type, image_name=image_name), width=ret_image.width, height=ret_image.height)
